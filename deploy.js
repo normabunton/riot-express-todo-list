7eb74254249f413d7f08b370c7f3120e92eca975
normabunton <te_nj25@hotmail.com>
2021-05-22 16:55:41 -0400

    updateurl

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.234.185.200',
+      host: '54.90.154.183',
       username: 'ubuntu',
       privateKey: 'new-key.pem'
     })
